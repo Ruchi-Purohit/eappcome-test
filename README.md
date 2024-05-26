a0bbb0b5fe1952e9a69675fe44838c4c38fb6070
Ruchi-Purohit <purohit12ruchi@gmail.com>
2024-05-26 11:55:19 -0500

    Fist Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# eappcome-test
---
+++ UnitTest1.cs
@@ -0,0 +1,43 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // Create a new instance of Selenium Webdriver
+            IWebDriver driver = new ChromeDriver();
+            // Navigate to URL
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            // Maximize the window
+            driver.Manage().Window.Maximize();
+            IWebElement webElement = driver.FindElement(By.Name("q"));
+            webElement.SendKeys("Selenium");
+            webElement.SendKeys(Keys.Return);
+        }
+        [Test]
+        public void EAPPSOMME()
+        {
+            // Create a new instance of Selenium Webdriver
+            IWebDriver driver = new ChromeDriver();
+            // Navigate to URL
+            driver.Navigate().GoToUrl("http://eaapp.somee.com");
+            IWebElement loginlink = driver.FindElement(By.LinkText("Login"));
+            loginlink.Click();
+           // IWebDriver txtUserName
+
+            
+            
+
+
+        }
+    }
+}
